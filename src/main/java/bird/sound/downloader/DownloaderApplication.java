63354cd1ad71d948a9580f640e19c3572d68a047
skywalkerAlex <alexdim.liak@gmail.com>
2021-05-27 23:04:52 +0300

    Call a rest service - Dummy

--- src/main/java/bird/sound/downloader/DownloaderApplication.java
+++ src/main/java/bird/sound/downloader/DownloaderApplication.java
@@ -3,11 +3,16 @@ package bird.sound.downloader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import bird.sound.downloader.controller.DownloaderController;
+
 @SpringBootApplication
 public class DownloaderApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DownloaderApplication.class, args);
+
+		DownloaderController.callTemp();
+
 	}
 
 }
